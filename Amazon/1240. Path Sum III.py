1f9219776bcc636ea88b340dc3c666ff20518723
Tian Feng <feng.txy@outlook.com>
2019-11-09 12:10:33 -0800

    Create 1240. Path Sum III.py

---
+++ Amazon/1240. Path Sum III.py
@@ -0,0 +1,34 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: 
+    @param sum: 
+    @return: the num of sum path
+    """
+    res = 0
+    def pathSum(self, root, target):
+        # write your code here
+        self.dfs(root, target, [])
+        return self.res
+
+    def dfs(self, root, target, path):
+        if not root:
+            return
+        
+        path.append(root.val)
+        for i in range(len(path)):
+            # print(path[i:], sum(path[i:]),  target)
+            if sum(path[i:]) == target:
+                self.res += 1
+        self.dfs(root.left, target, path)
+        self.dfs(root.right, target, path)
+        path.pop()
+        
+        
