e3c1c46eff02863aa98b960d0cc54fec8f739d05
Tian Feng <feng.txy@outlook.com>
2019-11-14 11:34:09 -0800

    Update 1192. Critical Connections in a Network.py

--- Amazon/1192. Critical Connections in a Network.py
+++ Amazon/1192. Critical Connections in a Network.py
@@ -1,50 +1,39 @@
 from collections import defaultdict
 class Solution:
     def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
-        graph = defaultdict(list)
+        curr_to_children = defaultdict(list)
         n = len(connections)
-        for x, y in connections:
-            graph[x].append(y)
-            graph[y].append(x)
+        for x,y in connections:
+            curr_to_children[x].append(y)
+            curr_to_children[y].append(x)
         visited = [0]*n
         dfn = [float('inf')]*n
         low = [float('inf')]*n
         parent = [-1]*n
         count = 0
-        
         result = []
-        
-        for i in range(n):
-            if not visited[i]:
-                self.tarjan(i,graph,visited,dfn,low,parent,count,result)
+        for node in range(n):
+            if not visited[node]:
+                self.tarjan(node, curr_to_children, visited, dfn, low, parent, count, result)
         return result
-    
-    def tarjan(self,curr,graph,visited,dfn,low,parent,count,result):
-
-        visited[curr]=True
-        dfn[curr]=count
-        low[curr]=count
+                
+    def tarjan(self, curr, curr_to_children, visited, dfn, low, parent, count, result):
+        visited[curr] = 1
+        dfn[curr] = count
+        low[curr] = count
         count += 1
-
-        for child in graph[curr]:
-            if visited[child]!=True:
-                parent[child]=curr
-
-                self.tarjan(child,graph,visited,dfn,low,parent,count,result)
-
-                low[curr]=min(low[curr],low[child])
-
-                if low[child]>dfn[curr]:
-                    result.append([curr,child])
-                    
+        
+        for child in curr_to_children[curr]:
+            if not visited[child]:
+                parent[child] = curr
+                self.tarjan(child, curr_to_children, visited, dfn, low, parent, count, result)
+                low[curr] = min(low[curr], low[child])
+                
+                if low[child] > dfn[curr]:
+                    result.append([curr, child])
                 # find critical point
                 # if low[child]>=dfn[curr]:
                 #     result.append(curr)
-
             if child != parent[curr]:
-                low[curr]=min(low[curr],dfn[child])
-                
-        
-        
-        
-        
+                low[curr] = min(low[curr], dfn[child])
+                    
