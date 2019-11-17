598c8321d50ea911827e4e1c8e230fc56d8aa15e
Tian Feng <feng.txy@outlook.com>
2019-11-16 17:35:49 -0800

    Update 468. Symmetric Binary Tree.py

--- Amazon/468. Symmetric Binary Tree.py
+++ Amazon/468. Symmetric Binary Tree.py
@@ -14,14 +14,14 @@ class Solution:
     def isSymmetric(self, root):
         # write your code here
         if not root:
-            return True 
+            return True  
         return self.helper(root.left, root.right)
             
         
-    def helper(self, left, right):
-        if not left and not right:
+    def helper(self, l, r):
+        if not l and not r:
             return True 
-        if left and right and left.val == right.val:
-            return self.helper(left.left, right.right) and self.helper(left.right, right.left)
+        if r and l and l.val == r.val:
+            return self.helper(l.left, r.right) and self.helper(l.right, r.left)
         return False 
         
