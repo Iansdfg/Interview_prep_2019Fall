51aec5ad201598e0aa51f575d440cb19dc42b6cc
Tian Feng <feng.txy@outlook.com>
2019-11-25 12:22:52 -0800

    Update 193. Longest Valid Parentheses.py

--- Amazon/193. Longest Valid Parentheses.py
+++ Amazon/193. Longest Valid Parentheses.py
@@ -6,6 +6,19 @@ class Solution:
     def longestValidParentheses(self, s):
         # write your code here
         # stack store start of first valid position - 1 AKA end of last unvalid position
+        stack = [-1]
+        max_len = 0
+        for i, char in enumerate(s):
+            if char == '(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    max_len = max(max_len, i-stack[-1])
+        return max_len
+                
         # stack = [-1]
         # max_len = 0
         # for i in range(len(s)):
@@ -19,21 +32,19 @@ class Solution:
         #             max_len = max(max_len, i-stack[-1])
                     
         # return max_len
-        return max(self.helper(s, '('), self.helper(s[::-1], ')') )
-        
+    #     return max(self.helper(s, '('), self.helper(s[::-1], ')') )
     
-    def helper(self, s, para):
-        curr, max_curr, max_len = 0, 0, 0
-        for char in s:
-            if char == para:
-                curr += 1  
-            else:
-                curr -= 1
-            max_curr += 1
-                
-            if curr == 0:
-                max_len = max(max_len, max_curr)
-            elif curr < 0 :
-                max_curr = 0
-                curr = 0
-        return max_len
+    # def helper(self, s, para):
+    #     curr, max_curr, max_len = 0, 0, 0
+    #     for char in s:
+    #         if char == para:
+    #             curr += 1  
+    #         else:
+    #             curr -= 1
+    #         max_curr += 1
+    #         if curr == 0:
+    #             max_len = max(max_len, max_curr)
+    #         elif curr < 0 :
+    #             max_curr = 0
+    #             curr = 0
+    #     return max_len
