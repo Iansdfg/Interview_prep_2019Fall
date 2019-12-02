b3cb73dd1327b5629d3bd00d3dfe0dfc3090f784
Tian Feng <feng.txy@outlook.com>
2019-12-01 17:06:34 -0800

    Update 114. Unique Paths.py

--- Amazon/114. Unique Paths.py
+++ Amazon/114. Unique Paths.py
@@ -10,12 +10,14 @@ class Solution:
             return 0
         dp = [[ 0 for _ in range(m + 1)] for _ in range(n + 1)]
         dp[1][1] = 1
-        print(dp)
+#         print(dp)
+        
         for row in range(1, len(dp)):
             for col in range(1, len(dp[0])):
                 if row == 1 and col == 1:
                     continue
                 dp[row][col] = dp[row-1][col] + dp[row][col-1] 
+                
         return dp[-1][-1]
                 
                 
