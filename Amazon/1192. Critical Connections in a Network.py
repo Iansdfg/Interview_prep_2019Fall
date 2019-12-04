324a3bc5e7d8d4783a4db8be47ba438229bc41c3
Tian Feng <feng.txy@outlook.com>
2019-12-04 10:45:34 -0800

    Update 1192. Critical Connections in a Network.py

--- Amazon/1192. Critical Connections in a Network.py
+++ Amazon/1192. Critical Connections in a Network.py
@@ -2,36 +2,41 @@ from collections import defaultdict
 class Solution:
     def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
         curr_to_children = defaultdict(list)
-        n = len(connections)
-        for x,y in connections:
+        for x, y in connections:
             curr_to_children[x].append(y)
             curr_to_children[y].append(x)
         visited = [0]*n
-        dfn = [float('inf')]*n
-        low = [float('inf')]*n
-        parent = [-1]*n
+        order = [-1]*n
+        low = [-1]*n
+        father = [-1]*n
+        res = []
         count = 0
-        result = []
         for node in range(n):
-            if not visited[node]:
-                self.tarjan(node, curr_to_children, visited, dfn, low, parent, count, result)
-        return result
-                
-    def tarjan(self, curr, curr_to_children, visited, dfn, low, parent, count, result):
-        visited[curr] = 1
-        dfn[curr] = count
-        low[curr] = count
-        count += 1
+            self.tarjan(node, curr_to_children, visited, order, low, father, count, res)
+        return res
         
-        for child in curr_to_children[curr]:
+    def tarjan(self, node, curr_to_children, visited, order, low, father, count, res):
+        visited[node] = 1
+        order[node] = count
+        low[node] = count
+        count += 1 
+        
+        for child in curr_to_children[node]:
             if not visited[child]:
-                parent[child] = curr
-                self.tarjan(child, curr_to_children, visited, dfn, low, parent, count, result)
-                low[curr] = min(low[curr], low[child])
+                father[child] = node
+                self.tarjan(child, curr_to_children, visited, order, low, father, count, res)
+                low[node] = min(low[node], low[child])
+
+                if order[node] < low[child]:
+                    res.append([node, child])
                 
-                if low[child] > dfn[curr]:
-                    result.append([curr, child])
-            if child != parent[curr]:
-                low[curr] = min(low[curr], dfn[child])
-                    
-                
+            if child != father[node]:
+                low[node] = min(low[node], order[child])
+            
+            
+            
+            
+            
+            
+            
+            
