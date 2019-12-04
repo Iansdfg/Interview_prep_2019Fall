f693655cfbcc0fd9da6f196919e6b7260c57b70f
Tian Feng <feng.txy@outlook.com>
2019-12-04 15:23:29 -0800

    Update 1192. Critical Connections in a Network.py

--- Amazon/1192. Critical Connections in a Network.py
+++ Amazon/1192. Critical Connections in a Network.py
@@ -1,42 +1,38 @@
 from collections import defaultdict
+
 class Solution:
     def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
-        curr_to_children = defaultdict(list)
+        curr_to_child = defaultdict(list)
         for x, y in connections:
-            curr_to_children[x].append(y)
-            curr_to_children[y].append(x)
+            curr_to_child[x].append(y)
+            curr_to_child[y].append(x)    
         visited = [0]*n
         order = [-1]*n
         low = [-1]*n
-        father = [-1]*n
-        res = []
-        count = 0
+        parent = [-1]*n
+        count = 1 
+        result = []
         for node in range(n):
-            self.tarjan(node, curr_to_children, visited, order, low, father, count, res)
-        return res
+            self.targan(node, curr_to_child, visited, order, low, parent, count, result)
+        return result
+    
+    def targan(self, curr, curr_to_child, visited, order, low, parent, count, result):
+        visited[curr] = 1
+        order[curr] = count
+        low[curr] = count
+        count += 1
         
-    def tarjan(self, node, curr_to_children, visited, order, low, father, count, res):
-        visited[node] = 1
-        order[node] = count
-        low[node] = count
-        count += 1 
-        
-        for child in curr_to_children[node]:
+        for child in curr_to_child[curr]:
             if not visited[child]:
-                father[child] = node
-                self.tarjan(child, curr_to_children, visited, order, low, father, count, res)
-                low[node] = min(low[node], low[child])
-
-                if order[node] < low[child]:
-                    res.append([node, child])
+                parent[child] = curr
+                self.targan(child, curr_to_child, visited, order, low, parent, count, result)
+                low[curr] = min(low[curr], low[child])
                 
-            if child != father[node]:
-                low[node] = min(low[node], order[child])
-            
-            
-            
-            
-            
-            
-            
-            
+                if order[curr] < low[child]:
+                    result.append([curr, child])
+                    
+            if parent[curr] != child:
+                low[curr] = min(low[curr], order[child])
+                
+                
+        
