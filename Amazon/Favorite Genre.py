51a05107f9d303109b7d1dad51623d1236502d74
Tian Feng <feng.txy@outlook.com>
2019-12-08 12:42:05 -0800

    Update Favorite Genre.py

--- Amazon/Favorite Genre.py
+++ Amazon/Favorite Genre.py
@@ -1,4 +1,4 @@
-def fav_genra(userMap, genreMap):
+def fav_genra_old(userMap, genreMap):
     result = { user: [] for user in userMap}
     if not userMap or not genreMap:
         return result 
@@ -11,19 +11,23 @@ def fav_genra(userMap, genreMap):
 
     for user in userMap:
         genra_to_cnt = dict()
-        # max_value = float('-inf')
+        max_value = float('-inf')
         for song in userMap[user]:
             genra = song_to_genra[song]
             genra_to_cnt[genra] = genra_to_cnt.get(genra, 0)+1
 
-            # max_value = max(genra_to_cnt[genra], max_value)
+            max_value = max(genra_to_cnt[genra], max_value)
 
-        # for genra in genra_to_cnt:
-        #     # print(genra, genra_to_cnt[genra], max_value)
-        #     if genra_to_cnt[genra] == max_value:
-        #         result[user].append(genra)
+        for genra in genra_to_cnt:
+            # print(genra, genra_to_cnt[genra], max_value)
+            if genra_to_cnt[genra] == max_value:
+                result[user].append(genra)
 
-        result[user] = [key for key, val in genra_to_cnt.items() if val == max(genra_to_cnt.values())]
+        
+        for genra in genra_to_cnt:
+            print(genra, genra_to_cnt[genra], max_value)
+            if genra_to_cnt[genra] == max_value:
+                result[user].append(genra)
             
     return result
 
@@ -72,49 +76,49 @@ if __name__ == '__main__':
         "Pop": ["song5", "song6"],
         "Jazz": ["song8", "song9"]
     }
-    # 2
-    userMap = {
-        "David": ["song1", "song2"],
-        "Emma": ["song3", "song4"]
-    }
-    genreMap = {}
-    # 3
-    userMap = {
-    }
-    genreMap = {}
-    # 4
-    userMap = {
-    }
-    genreMap = {
-        "Rock": ["song1", "song3"],
-        "Dubstep": ["song7"],
-        "Techno": ["song2", "song4"],
-        "Pop": ["song5", "song6"],
-        "Jazz": ["song8", "song9"]
-    }
-    # 5
-    userMap = {"David": [],
-               "Emma": []
-    }
+    # # 2
+    # userMap = {
+    #     "David": ["song1", "song2"],
+    #     "Emma": ["song3", "song4"]
+    # }
+    # genreMap = {}
+    # # 3
+    # userMap = {
+    # }
+    # genreMap = {}
+    # # 4
+    # userMap = {
+    # }
+    # genreMap = {
+    #     "Rock": ["song1", "song3"],
+    #     "Dubstep": ["song7"],
+    #     "Techno": ["song2", "song4"],
+    #     "Pop": ["song5", "song6"],
+    #     "Jazz": ["song8", "song9"]
+    # }
+    # # 5
+    # userMap = {"David": [],
+    #            "Emma": []
+    # }
 
-    genreMap = {
-        "Rock": ["song1", "song3"],
-        "Dubstep": ["song7"],
-        "Techno": ["song2", "song4"],
-        "Pop": ["song5", "song6"],
-        "Jazz": ["song8", "song9"]
-    }
-    # 6
-    userMap = {"David": ["song1", "song2", "song3", "song4", "song8"],
-               "Emma": ["song5", "song6", "song7"]
-    }
+    # genreMap = {
+    #     "Rock": ["song1", "song3"],
+    #     "Dubstep": ["song7"],
+    #     "Techno": ["song2", "song4"],
+    #     "Pop": ["song5", "song6"],
+    #     "Jazz": ["song8", "song9"]
+    # }
+    # # 6
+    # userMap = {"David": ["song1", "song2", "song3", "song4", "song8"],
+    #            "Emma": ["song5", "song6", "song7"]
+    # }
 
-    genreMap = {
-        "Rock": [],
-        "Dubstep": [],
-        "Techno": [],
-        "Pop": [],
-        "Jazz": []
-    }
+    # genreMap = {
+    #     "Rock": [],
+    #     "Dubstep": [],
+    #     "Techno": [],
+    #     "Pop": [],
+    #     "Jazz": []
+    # }
 
     print(fav_genra(userMap, genreMap))
