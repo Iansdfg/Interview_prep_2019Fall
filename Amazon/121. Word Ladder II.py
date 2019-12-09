21a6f97f7e73a845d94a4dc36816084567413aba
Tian Feng <feng.txy@outlook.com>
2019-12-09 15:02:00 -0800

    Update 121. Word Ladder II.py

--- Amazon/121. Word Ladder II.py
+++ Amazon/121. Word Ladder II.py
@@ -11,40 +11,43 @@ class Solution:
         dict.add(start)
         dict.add(end)
         distance = {}
+        self.bfs(end, distance, dict)
+        print(distance)
+        result = []
+        self.dfs(start, end, distance, dict, [start], result)
         
-        self.bfs(end, start, distance, dict)
-        results = []
-        self.dfs(start, end, distance, dict, [start], results)
-        return results
+        return result
         
-    def bfs(self, start, end, distance, dict):
+    def bfs(self, start, distance, dict):
         distance[start] = 0
         queue = deque([start])
         while queue:
             word = queue.popleft()
-            for next_word in self.get_next(word, dict):
+            for next_word in self.get_next_word(word, dict):
                 if next_word not in distance:
-                    distance[next_word] = distance[word]+1
+                    distance[next_word] = distance[word] + 1
                     queue.append(next_word)
                     
-    def get_next(self, word, dict):
+    def get_next_word(self, word, dict):
         words = []
         for i in range(len(word)):
-            for c in "abcdefghijklmnopqrstuvwxyz":
-                next_word = word[:i] + c + word[i + 1:]
+            for char in 'abcdefghijklmnopqrstuvwxyz':
+                next_word = word[:i]+char+word[i+1:]
                 if next_word != word and next_word in dict:
                     words.append(next_word)
         return words
-    
-    
-    def dfs(self, curr, target, distance, dict, path, results):
-        if curr == target:
-            results.append(list(path))
+                
+                
+    def dfs(self, curt, target, distance, dict, path, result):
+        if curt == target:
+            result.append(path[:])
             return
         
-        for word in self.get_next(curr, dict):
-            if distance[word]!=distance[curr]-1:
+        for word in self.get_next_word(curt, dict):
+            if distance[word] != distance[curt] - 1:
                 continue
             path.append(word)
-            self.dfs(word, target, distance, dict, path, results)
+            self.dfs(word, target, distance, dict, path, result)
             path.pop()
+        
+   
