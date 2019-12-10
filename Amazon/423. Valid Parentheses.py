089ce7ef1ed7edc261a7a04d8a1d11cc2f1941c3
Tian Feng <feng.txy@outlook.com>
2019-12-10 15:19:37 -0800

    Create 423. Valid Parentheses.py

---
+++ Amazon/423. Valid Parentheses.py
@@ -0,0 +1,27 @@
+class Solution:
+    """
+    @param s: A string
+    @return: whether the string is a valid parentheses
+    """
+    def isValidParentheses(self, s):
+        # write your code here
+        stack = []
+        
+        for char in s:
+            if char in "([{":
+                stack.append(char)
+            else:
+                if not stack:
+                    return False
+                    
+                if char == ')' and stack[-1]!='(':
+                    return False 
+                if char == '}' and stack[-1]!='{':
+                    return False 
+                if char == ']' and stack[-1]!='[':
+                    return False 
+                stack.pop()
+                
+        return  stack == []
+        
+        
