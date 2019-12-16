73ed0e374ebba0e086ff7ebd06d3a5bf02455de5
Tian Feng <feng.txy@outlook.com>
2019-12-16 11:24:23 -0800

    Update 1283. Reverse String.py

--- Amazon/1283. Reverse String.py
+++ Amazon/1283. Reverse String.py
@@ -6,6 +6,8 @@ class Solution:
     def reverseString(self, s):
         # write your code here
         res = []
+        
         for i in range(len(s)):
             res.append(s[len(s)-i-1])
         return ''.join(res)
+   
