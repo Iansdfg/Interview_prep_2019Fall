b5f8f0ac82a549f7627872282bc1910b2dd5b354
Tian Feng <feng.txy@outlook.com>
2019-12-21 21:19:12 -0800

    Update 1369. Most Common Word.py

--- Amazon/1369. Most Common Word.py
+++ Amazon/1369. Most Common Word.py
@@ -18,13 +18,16 @@ class Solution:
                 word_dic[word] += 1
             else:
                 word_dic[word] = 1
+                
         max_word = ''
         max_time = 0
+        
         for key in word_dic:
             if key not in banned:
                 time = word_dic[key]
                 max_time = max(time, max_time)
                 if time == max_time:
                     max_word = key
+                    
         return max_word
             
