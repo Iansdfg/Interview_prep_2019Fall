6f0911a949fd7e92e14a51d629afd1172c9fa64e
Tian Feng <feng.txy@outlook.com>
2019-12-24 10:22:44 -0800

    Update 52. Next Permutation.py

--- Amazon/52. Next Permutation.py
+++ Amazon/52. Next Permutation.py
@@ -5,23 +5,18 @@ class Solution:
     """
     def nextPermutation(self, nums):
         # write your code here
-
         if sorted(nums[:])[::-1] == nums:
             return nums[::-1]
-        
-        
         first_down, slightly_large= len(nums)-1, len(nums)-1
 
         for first_down in range(len(nums)-2, -1, -1):
             if nums[first_down] < nums[first_down + 1]:
                 break 
-
         for slightly_large in range(len(nums)-1, first_down, -1):
             if nums[slightly_large] > nums[first_down]:
                 nums[slightly_large], nums[first_down] = nums[first_down], nums[slightly_large]
                 break
         nums = nums[:first_down+1] + nums[first_down+1:][::-1]
-        
         return nums
             
         
