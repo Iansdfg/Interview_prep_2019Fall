9cd8b3e17c79ee049de29a02fbc1caf4b97ec225
Tian Feng <feng.txy@outlook.com>
2020-01-30 10:45:11 -0800

    Update 240. Search a 2D Matrix II.py

--- Amazon/240. Search a 2D Matrix II.py
+++ Amazon/240. Search a 2D Matrix II.py
@@ -5,16 +5,17 @@ class Solution:
         :type target: int
         :rtype: bool
         """
-        if len(matrix) == 0 or len(matrix[0]) == 0:
+        if matrix == [] or matrix[0] == []:
             return False
         rows, cols = len(matrix), len(matrix[0])
-        row, col = len(matrix)-1, 0
+        row, col = rows-1, 0
         while row >= 0 and col < cols:
-            if matrix[row][col] == target:
-                return True
-            elif matrix[row][col] < target:
+            if matrix[row][col] < target:
                 col += 1
             elif matrix[row][col] > target:
                 row -= 1
+            else:
+                return True 
         return False
-         
+            
+        
