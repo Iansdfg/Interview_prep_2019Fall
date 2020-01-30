252461b14e1a4b52b395c1bcf793bdb940e53ad7
Tian Feng <feng.txy@outlook.com>
2020-01-30 09:44:30 -0800

    Update 21. Merge Two Sorted Lists.py

--- Amazon/21. Merge Two Sorted Lists.py
+++ Amazon/21. Merge Two Sorted Lists.py
@@ -6,14 +6,15 @@
 
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        curr = new_head = ListNode(-1)
-        while l1 and l2:
-            if l1.val < l2.val:
-                curr.next = ListNode(l1.val)
-                l1 = l1.next
-            else:
-                curr.next = ListNode(l2.val)
-                l2 = l2.next
-            curr = curr.next
-        curr.next = l1 or l2
-        return new_head.next
+        if not l1:
+            return l2
+        elif not l2:
+            return l1 
+        elif l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        elif l1.val >= l2.val:
+            l2.next =  self.mergeTwoLists(l1, l2.next)
+            return l2
+        
+        
