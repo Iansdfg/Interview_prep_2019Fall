0dcc6fd1243948a24502b78876dc27a58e62a6a5
Tian Feng <feng.txy@outlook.com>
2020-02-17 16:33:41 -0800

    Create 116. Jump Game.py

---
+++ Amazon/116. Jump Game.py
@@ -0,0 +1,19 @@
+class Solution:
+    """
+    @param A: A list of integers
+    @return: A boolean
+    """
+    def canJump(self, A):
+        # write your code here
+        dp = [0 for _ in range(len(A))]
+        dp[0] = 1
+        
+        for i in range(len(A)):
+            if not dp[i]:
+                continue
+            steps = A[i]
+            for step in range(1, steps+1):
+                if i + step >= len(dp) or dp[i + step]:
+                    continue
+                dp[i + step] = 1 
+        return bool(dp[-1]) 
