ac3d636d4d1a3a3bee70f95fb6df3791b28be0b4
Tian Feng <feng.txy@outlook.com>
2020-02-24 15:27:19 -0800

    Update 1221. Concatenated Words.py

--- Amazon/1221. Concatenated Words.py
+++ Amazon/1221. Concatenated Words.py
@@ -11,20 +11,22 @@ class Solution:
         for i in range(0, len(words)):
             cands -= {words[i]}
             if self.wordBreak(words[i], cands):
+                if words[i] == '':
+                    continue
                 ans.append(words[i]) 
         return ans
 
-    def wordBreak(self, word, cands):
-        if not cands:
-            return False
-        dp = [False] * (len(word) + 1) #store whether w.substr(0, i) can be formed by existing words
-        dp[0] = True #empty string is always valid
-        for i in range(1, len(word) + 1):
-            for j in reversed(range(0, i)):
+    def wordBreak(self, s, dict):
+        # write your code here
+        dict_set = set(dict)
+        n = len(s)
+        dp = [0] * (n + 1)
+        dp[0] = 1 
+        for i in range(n + 1):
+            for j in range(i + 1):
                 if not dp[j]:
                     continue
-                if word[j:i] in cands:
-                    dp[i] = True
-                    break
-        return dp[-1]
- 
+                if s[j:i] in dict:
+                    dp[i] = 1 
+                    break 
+        return bool(dp[-1])
