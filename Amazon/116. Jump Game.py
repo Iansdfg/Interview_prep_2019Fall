ce04a83adb436274a6c87bf3f27cb6a7ef21d065
Tian Feng <feng.txy@outlook.com>
2020-03-02 17:56:02 -0800

    Update 116. Jump Game.py

--- Amazon/116. Jump Game.py
+++ Amazon/116. Jump Game.py
@@ -11,9 +11,11 @@ class Solution:
         for i in range(len(A)):
             if not dp[i]:
                 continue
+                
             steps = A[i]
             for step in range(1, steps+1):
                 if i + step >= len(dp) or dp[i + step]:
                     continue
                 dp[i + step] = 1 
+                
         return bool(dp[-1]) 
