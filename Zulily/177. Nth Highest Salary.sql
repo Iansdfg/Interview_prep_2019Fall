b0ef8ad9fe1dd950c7881984243243d7796246ba
Tian Feng <feng.txy@outlook.com>
2020-03-12 12:19:37 -0700

    Update 177. Nth Highest Salary.sql

--- Zulily/177. Nth Highest Salary.sql
+++ Zulily/177. Nth Highest Salary.sql
@@ -1,16 +1,14 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-  SET N = N - 1;
-  RETURN (
-      # Write your MySQL query statement below. 
-      SELECT (
+    SET N = N - 1;
+    RETURN (
+      # Write your MySQL query statement below.
+      SELECT 
         IFNULL(
-            (SELECT DISTINCT Salary 
-            FROM Employee 
-            ORDER BY Salary DESC
-            LIMIT 1 OFFSET N),
-            NULL
-        )
-      ) AS NthHighestSalary    
+            (SELECT DISTINCT Salary
+             FROM Employee
+             ORDER BY Salary DESC
+             LIMIT 1 OFFSET N)
+        , NULL) AS NthHighestSalary
   );
 END
