f79a562dfde0ec5cd530bdd73adc7f344105da0d
Tian Feng <feng.txy@outlook.com>
2020-03-12 11:20:14 -0700

    Update 175. Combine Two Tables.sql

--- Zulily/175. Combine Two Tables.sql
+++ Zulily/175. Combine Two Tables.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-SELECT FirstName, LastName, City, State 
+SELECT FirstName, LastName, City, State
 FROM Person
 LEFT JOIN Address
-ON Person.PersonId = Address.PersonId;
+ON Person.PersonId = Address.PersonId
