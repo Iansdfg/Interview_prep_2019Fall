95790e228b1d53a602933b41d0a4aca1ad51d720
Tian Feng <feng.txy@outlook.com>
2020-03-16 10:54:23 -0700

    Update 184. Department Highest Salary.sql

--- Zulily/184. Department Highest Salary.sql
+++ Zulily/184. Department Highest Salary.sql
@@ -1,17 +1,20 @@
 # Write your MySQL query statement below
 SELECT 
-    Department.Name AS Department,
-    Employee.Name AS Employee,
-    Employee.Salary AS Salary
-FROM Employee
-LEFT JOIN Department
-ON Employee.DepartmentId = Department.Id
-WHERE
-    (Department.Name, Employee.Salary) IN
-        (SELECT Department.Name, MAX(Salary)
-        FROM Employee
-        LEFT JOIN Department
-        ON Employee.DepartmentId = Department.Id
-        GROUP BY Department.Name)
-        
-
+    DEP_EMP.DEP_NAME AS Department, 
+    DEP_EMP.EMP_NAME AS Employee, 
+    DEP_EMP.Salary AS Salary
+FROM
+    (SELECT 
+        Department.NAME AS DEP_NAME, 
+        Department.id AS DEP_ID, 
+        Employee.Name AS EMP_NAME, 
+        Salary
+    FROM Employee
+    LEFT JOIN Department
+    ON Employee.DepartmentId = Department.Id) AS DEP_EMP
+LEFT JOIN
+    (SELECT DepartmentId, max(Salary) AS MAX_SAL
+    FROM Employee
+    GROUP BY DepartmentId) AS ID_HIGHEST
+ON DEP_EMP.DEP_ID = ID_HIGHEST.DepartmentId
+WHERE DEP_EMP.Salary = ID_HIGHEST.MAX_SAL
