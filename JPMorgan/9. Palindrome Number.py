d0fe7b7cde0650eb81cdc48d18bd27b305d6587c
Tian Feng <feng.txy@outlook.com>
2020-03-22 15:47:17 -0700

    Create 9. Palindrome Number.py

---
+++ JPMorgan/9. Palindrome Number.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        res = 0
+        temp = x
+        while temp:
+            digit = temp % 10
+            res += digit
+            res *= 10
+            temp //= 10
+        return res//10 == x
+        
+            
+
+        
