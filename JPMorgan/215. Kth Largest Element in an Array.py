021ac1bdabaa67d4ca44b96d6fd7a3bdaa220cc1
Tian Feng <feng.txy@outlook.com>
2020-03-24 09:58:21 -0700

    Update 215. Kth Largest Element in an Array.py

--- JPMorgan/215. Kth Largest Element in an Array.py
+++ JPMorgan/215. Kth Largest Element in an Array.py
@@ -12,3 +12,48 @@ class Solution(object):
             top = heappop(nums)
             k -= 1
         return top
+    
+    
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        start, end = 0, len(nums) - 1
+        return self.helper(nums, k - 1, start, end)
+    
+    def helper(self, nums, k, start, end):
+        # return kth largest
+        if start >= end:
+            return nums[start]
+        
+        left, right = start, end
+        pivit = nums[(left + right)//2]
+        
+        while left <= right:
+            while left <= right and nums[left] > pivit:
+                left += 1 
+            while left <= right and nums[right] < pivit:
+                right -= 1
+            if left <= right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1 
+                right -= 1
+        
+        if k <= right:
+            return self.helper(nums, k, start, right)
+        elif k >= left:
+            return self.helper(nums, k, left, end)
+        else:
+            return nums[k]
+            
+                
+                
+                
+                
+                
+                
+                
+        
