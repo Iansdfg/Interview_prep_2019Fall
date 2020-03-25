5299dee5150bb4fd67ed53bc372680ce7275a2a2
Tian Feng <feng.txy@outlook.com>
2020-03-25 09:29:05 -0700

    Update 146. LRU Cache.py

--- JPMorgan/146. LRU Cache.py
+++ JPMorgan/146. LRU Cache.py
@@ -1,49 +1,54 @@
 class LinkedNode:
-    def __init__(self, val = None, next = None, key = None):
+    def __init__(self, val = None, key = None, next = None):
         self.val = val
-        self.next = next
         self.key = key
+        self.next = next
         
 class LRUCache:
+
     def __init__(self, capacity: int):
         self.key_to_prev = dict()
         self.capacity = capacity
         self.dummy = LinkedNode()
         self.tail = self.dummy
-
+        
     def get(self, key: int) -> int:
         if key not in self.key_to_prev:
-            return -1 
+            return -1
+        res = self.key_to_prev[key].next.val
         self.kick(self.key_to_prev[key])
-        return self.key_to_prev[key].next.val
+        return res
         
     def put(self, key: int, value: int) -> None:
         if key in self.key_to_prev:
-            self.kick(self.key_to_prev[key])
             self.key_to_prev[key].next.val = value
+            self.kick(self.key_to_prev[key])
         else:
-            self.append(LinkedNode(value, None ,key))
+            new = LinkedNode(value, key, None)
+            self.append(new)
             if len(self.key_to_prev) > self.capacity:
-                self.pop_fornt()
-        
+                self.pop_front()
+                
     def kick(self, prev):
         node = prev.next
         if node == self.tail:
             return 
         
-        nextt = node.next 
+        nextt = node.next
         prev.next = nextt
         if nextt:
             self.key_to_prev[nextt.key] = prev
+        
         self.append(node)
+        
         
     def append(self, node):
         self.tail.next = node
         self.key_to_prev[node.key] = self.tail
-        self.tail = node
+        self.tail = node 
         
-    def pop_fornt(self, ):
-        head = self.dummy.next 
+    def pop_front(self, ):
+        head = self.dummy.next
         self.dummy.next = head.next
         self.key_to_prev[head.next.key] = self.dummy
         del self.key_to_prev[head.key]
