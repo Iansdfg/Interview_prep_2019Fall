8b585dcc53253d311d5e58722bc6e197fdb220ef
Tian Feng <feng.txy@outlook.com>
2020-03-25 09:45:08 -0700

    Update 17. Letter Combinations of a Phone Number.py

--- JPMorgan/17. Letter Combinations of a Phone Number.py
+++ JPMorgan/17. Letter Combinations of a Phone Number.py
@@ -9,6 +9,7 @@ class Solution(object):
         '8':'tuv',
         '9':'wxyz',
     }
+    
     def letterCombinations(self, digits):
         """
         :type digits: str
@@ -16,21 +17,20 @@ class Solution(object):
         """
         if not digits:
             return []
-        res = []
-        self.dfs(digits, 0, [], res)
-        return res 
+        results = []
+        self.dfs(digits, 0, [], results)
+        return results
     
-    def dfs(self, digits, index, path, res):
-        if index == len(digits) and len(path) == len(digits):
-            res.append(''.join(path))
+    def dfs(self, digits, index, path, results):
+        if len(path) == len(digits):
+            results.append(''.join(path))
             return 
-        for dig_i in range(index, len(digits)):
-            chars = self.dig_to_char[digits[dig_i]]
+        for dig_pos in range(index, len(digits)):
+            chars = self.dig_to_char[digits[dig_pos]]
             for char in chars:
                 path.append(char)
-                self.dfs(digits, dig_i + 1, path, res)
+                self.dfs(digits, dig_pos + 1, path, results)
                 path.pop()
-            
                 
         
         
