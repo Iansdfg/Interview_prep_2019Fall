ced50aacae821eab9454127f47b0f0f0a181c58f
Tian Feng <feng.txy@outlook.com>
2020-03-25 00:21:55 -0700

    Update 215. Kth Largest Element in an Array.py

--- JPMorgan/215. Kth Largest Element in an Array.py
+++ JPMorgan/215. Kth Largest Element in an Array.py
@@ -13,7 +13,6 @@ class Solution(object):
             k -= 1
         return top
     
-    
 class Solution(object):
     def findKthLargest(self, nums, k):
         """
@@ -22,29 +21,31 @@ class Solution(object):
         :rtype: int
         """
         start, end = 0, len(nums) - 1
-        return self.helper(nums, k - 1 , start, end)
+        return self.helper(nums, k - 1, start, end)
         
     def helper(self, nums, k, start, end):
-        if start >= end:
+        if start == end:
             return nums[start]
+        
         left, right = start, end
         pivot = nums[(left + right)//2]
+        
         while left <= right:
             while left <= right and nums[left] > pivot:
-                left += 1 
+                left += 1
             while left <= right and nums[right] < pivot:
-                right -= 1
-            if  left <= right:
-                nums[left], nums[right] = nums[right], nums[left]
+                right -= 1 
+            if left <= right:
+                nums[left], nums[right] = nums[right],nums[left]
                 left += 1 
                 right -= 1
             
         if k <= right:
-            return self.helper(nums, k , start, right)
+            return self.helper(nums, k, start, right)
         elif k >= left:
-            return self.helper(nums, k , left, end)
+            return self.helper(nums, k, left, end)
         else:
             return nums[k]
-                
+        
                 
         
