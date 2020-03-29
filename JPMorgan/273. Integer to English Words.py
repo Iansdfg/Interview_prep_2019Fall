3432c81e6197fa928a5e0f09f5c92fc9e0dca2cc
Tian Feng <feng.txy@outlook.com>
2020-03-29 00:33:54 -0700

    Update 273. Integer to English Words.py

--- JPMorgan/273. Integer to English Words.py
+++ JPMorgan/273. Integer to English Words.py
@@ -7,7 +7,9 @@ class Solution(object):
         :type num: int
         :rtype: str
         """
-        return ' '.join(self.words(num)) or 'Zero'
+        word_list = self.words(num)
+        res = ' '.join(word_list) if num else 'Zero'
+        return res
     
     def words(self, n):
             if n < 20:
@@ -16,6 +18,20 @@ class Solution(object):
                 return [self.tens[n/10-2]] + self.words(n%10)
             if n < 1000:
                 return [self.to19[n/100-1]] + ['Hundred'] + self.words(n%100)
-            for p, w in enumerate(('Thousand', 'Million', 'Billion'), 1):
-                if n < 1000**(p+1):
-                    return self.words(n/1000**p) + [w] + self.words(n%1000**p)
+            if n < 1000**2:
+                    return self.words(n/1000) + ['Thousand'] + self.words(n%1000)
+            if n < 1000**3:
+                    return self.words(n/1000**2) + ['Million'] + self.words(n%1000**2)
+            if n < 1000**4:
+                    return self.words(n/1000**3) + ['Billion'] + self.words(n%1000**3)
+                
+#             for p, w in enumerate(('Thousand', 'Million', 'Billion'), 1):
+#                 if n < 1000**(p+1):
+#                     return self.words(n/1000**p) + [w] + self.words(n%1000**p)
+                
+                
+                
+                
+                
+                
+                
