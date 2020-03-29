3ddf08b8c3e364b5c64b2fb0cc52d118baf5d70e
Tian Feng <feng.txy@outlook.com>
2020-03-29 12:37:37 -0700

    Update 122. Best Time to Buy and Sell Stock II.py

--- JPMorgan/122. Best Time to Buy and Sell Stock II.py
+++ JPMorgan/122. Best Time to Buy and Sell Stock II.py
@@ -4,27 +4,15 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        if not prices:
-            return 0
-        i = 0
         max_profit = 0
-        valley, peak = prices[0], prices[0]
-        while i < len(prices) - 1:
-            while i < len(prices) - 1 and prices[i] >= prices[i + 1]:
+        i = 0
+        while i + 1 <= len(prices) - 1:
+            while i + 1 <= len(prices) - 1 and prices[i] >= prices[i + 1]:
                 i += 1 
             valley = prices[i]
-            
-            while i < len(prices) - 1 and prices[i] <= prices[i + 1]:
-                i += 1
+            while i + 1 <= len(prices) - 1 and prices[i] <= prices[i + 1]:
+                i += 1 
             peak = prices[i]
-            print(valley, peak)
             max_profit += (peak - valley)
-        return max_profit 
+        return max_profit
             
-            
-            
-            
-            
-
-            
-        
