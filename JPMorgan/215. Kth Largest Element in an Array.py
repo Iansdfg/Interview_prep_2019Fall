04b1e840271b2244424e6e66a8e80161a1ea6339
Tian Feng <feng.txy@outlook.com>
2020-03-30 12:24:33 -0700

    Update 215. Kth Largest Element in an Array.py

--- JPMorgan/215. Kth Largest Element in an Array.py
+++ JPMorgan/215. Kth Largest Element in an Array.py
@@ -1,4 +1,3 @@
-from heapq import heapify, heappop, heappush
 class Solution(object):
     def findKthLargest(self, nums, k):
         """
@@ -6,25 +5,12 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        k = len(nums) - k + 1
-        heapify(nums)
-        while k:
-            top = heappop(nums)
-            k -= 1
-        return top
-    
-class Solution(object):
-    def findKthLargest(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        start, end = 0, len(nums) - 1
-        return self.helper(nums, k - 1, start, end)
+        start = 0 
+        end = len(nums) - 1
+        return self.helprt(nums, k - 1, start, end)
         
-    def helper(self, nums, k, start, end):
-        if start == end:
+    def helprt(self, nums, k, start, end):
+        if start >= end:
             return nums[start]
         
         left, right = start, end
@@ -34,18 +20,18 @@ class Solution(object):
             while left <= right and nums[left] > pivot:
                 left += 1
             while left <= right and nums[right] < pivot:
-                right -= 1 
-            if left <= right:
-                nums[left], nums[right] = nums[right],nums[left]
-                left += 1 
                 right -= 1
-            
+            if left <= right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+                
+                
         if k <= right:
-            return self.helper(nums, k, start, right)
+            return self.helprt(nums, k, start, right)
         elif k >= left:
-            return self.helper(nums, k, left, end)
+            return self.helprt(nums, k, left, end)
         else:
             return nums[k]
-        
                 
-        
+  
