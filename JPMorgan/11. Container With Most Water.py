56a0f3ba5411fcfef834ebc4def20d479f970a8d
Tian Feng <feng.txy@outlook.com>
2020-03-30 14:44:04 -0700

    Create 11. Container With Most Water.py

---
+++ JPMorgan/11. Container With Most Water.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        res = 0
+        left, right = 0, len(height) - 1
+        while left < right:
+            val = (right - left) * min(height[left], height[right])
+            res = max(val, res)
+            if height[left] < height[right]:
+                left += 1 
+            else:
+                right -= 1
+        return res
