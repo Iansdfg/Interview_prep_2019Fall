76c476fa729924de2c2254076d2966465b300390
Tian Feng <feng.txy@outlook.com>
2020-04-05 11:47:20 -0700

    Update 91. Decode Ways.py

--- Cisco/91. Decode Ways.py
+++ Cisco/91. Decode Ways.py
@@ -12,3 +12,17 @@ class Solution(object):
             if i != 1 and '09' < s[i - 2:i] < '27':
                 dp[i] += dp[i - 2]
         return dp[-1]
+    
+    def numDecodings(self, s):
+        if s=="" or s[0]=='0': return 0
+        dp=[1,1]
+        for i in range(2,len(s)+1):
+            if 10 <=int(s[i-2:i]) <=26 and s[i-1]!='0':
+                dp.append(dp[i-1]+dp[i-2])
+            elif int(s[i-2:i])==10 or int(s[i-2:i])==20:
+                dp.append(dp[i-2])
+            elif s[i-1]!='0':
+                dp.append(dp[i-1])
+            else:
+                return 0
+        return dp[len(s)]
